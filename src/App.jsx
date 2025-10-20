88c409d22f244b40d881943df05611acc4f4865e
mariamaelmi <151776162+mariamaelmi@users.noreply.github.com>
2025-10-20 14:16:52 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'; 
+import Greeting from './components/greeting';
+import Header from './components/Header';
+
+function App() {
+  return (
+    <>
+    <Greeting name ="Mariama" surname ="Elmi" />
+     <Header/>
+    
+         
+    </>
+  )
+}
+export default App
---
+++ src/components/greeting.jsx
@@ -0,0 +1,4 @@
+ function Greeting(props) {
+return <h1>Hello {props.name} {props.surname}.</h1>;
+  }
+  export default Greeting;
